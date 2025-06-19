531a5177a3d389e6e92a9b47aca23ecd3d50850c
Võ Minh Tuấn <vominhtuan09129@gmail.com>
2025-06-19 17:39:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const mineflayer = require('mineflayer');
+const Vec3 = require('vec3');
 const express = require('express');
 const app = express();
 
-// Web server để Railway không sleep
 app.get('/', (req, res) => res.send("✅ Bot đang chạy..."));
 app.listen(3000, () => console.log("🌐 Web server online."));
 
 function createBot() {
   const bot = mineflayer.createBot({
-    host: "letmecookVN.aternos.me", // đổi thành host server bạn
-    port: 46967,                 // và đúng port Aternos
-    username: "AFK_Bot",         // tên bot (TLauncher ok)
-    version: false               // auto detect version
+    host: "letmecookVN.aternos.me",
+    port: 46967,
+    username: "AFK_Bot",
+    version: false
   });
 
   bot.on('spawn', () => {
     console.log("✅ Bot đã vào server!");
 
-    // Nhảy mỗi 30 giây để chống AFK
+    // Nhảy nhẹ chống AFK
     setInterval(() => {
       bot.setControlState('jump', true);
       setTimeout(() => bot.setControlState('jump', false), 300);
-      console.log("⬆️ Bot vừa nhảy để chống AFK...");
-    }, 30000);
+      console.log("⬆️ Nhảy chống AFK...");
+    }, 10000);
   });
 
   bot.on('end', () => {
@@ -31,7 +31,7 @@ function createBot() {
   });
 
   bot.on('error', (err) => {
-    console.log(`❗ Lỗi: ${err.message}`);
+    console.log(`❗ Lỗi: ${err}`);
   });
 }
 
